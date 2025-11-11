310d2b7e782a6cc029b022c2082f67b095e271de
vian-rsperpam <gusviyan@gmail.com>
2025-11-11 13:33:07 +0700

    change beta notes

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
 <footer class="footer">
     <p>© 2025 Gusviyan - IT Dept RS Permata Pamulang | All Right Reserved</p>
+    <p>Apps Masih Dalam Tahap Uji Coba, Harap Melapor Ke Dept IT Bila Ditemukan Ketidaksesuaian</p>
 </footer>
