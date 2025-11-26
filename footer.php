b740a52c9e1c5a5f4cd4d38001ffcf1555c59f32
vian-rsperpam <gusviyan@gmail.com>
2025-11-26 12:05:26 +0700

    adjust footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    <a href=https://gusviyan.github.io/portofolio/" target="_blank" rel="noopener noreferrer">© 2025 Gusviyan - IT Dept RS Permata Pamulang | All Rights Reserved</a>
+    <a href=https://gusviyan.github.io/portofolio/ target="_blank" rel="noopener noreferrer">© 2025 Gusviyan - IT Dept RS Permata Pamulang | All Rights Reserved</a>
 </footer>
