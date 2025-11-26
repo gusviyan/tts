87a272331a31eb020cd0222bb73bca92dbdba97e
vian-rsperpam <gusviyan@gmail.com>
2025-11-26 11:54:57 +0700

    link footer to github porto

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    <p>© 2025 Gusviyan - IT Dept RS Permata Pamulang | All Right Reserved</p>
+    <a href=https://gusviyan.github.io/portofolio/" target="_blank" rel="noopener noreferrer">© 2025 Gusviyan - IT Dept RS Permata Pamulang | All Rights Reserved</a>
 </footer>
